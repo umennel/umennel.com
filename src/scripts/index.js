db2ae37b16a48d388341c01c1b3f4e56cdaa7d8b
Uche Mennel <umennel@gmx.ch>
2021-05-29 00:39:01 +0200

    Change navbar background on scroll

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,3 +4,8 @@ document.addEventListener('DOMContentLoaded', function() {
   M.Sidenav.init(document.querySelectorAll('.sidenav'));
   M.Parallax.init(document.querySelectorAll('.parallax'));
 });
+
+window.onscroll = function() {
+  document.getElementById("navbar").style.background =
+    (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) ? "#212121" : "none";
+}
